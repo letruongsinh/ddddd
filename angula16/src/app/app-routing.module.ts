5ccb84b71e34355d9dbc4dfe6cabbb69b73e294e
letruongsinh <118181636+letruongsinh@users.noreply.github.com>
2023-11-06 17:37:17 +0700

    sinhcomit

--- angula16/src/app/app-routing.module.ts
+++ angula16/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutComponent } from './layout/layout.component';
+import { ProductlistComponent } from './page/productlist/productlist.component';
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'product', component: ProductlistComponent },
+    ],
+  }]
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+  })
+  export class AppRoutingModule { }
--- angula16/src/app/app.module.ts
+++ angula16/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LayoutComponent } from './layout/layout.component';
+import { ProductlistComponent } from './page/productlist/productlist.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+
+         LayoutComponent,
+               ProductlistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
